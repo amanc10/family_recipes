4a39f37d03b77ca3b43088b0479e8fbffc898ea1
aman <qdaicheng@163.com>
2025-06-20 11:12:46 +0800

    Update WxCloudRunApplication.java

--- src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
+++ src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
@@ -1,14 +1,17 @@
 package com.tencent.wxcloudrun;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 @MapperScan(basePackages = {"com.tencent.wxcloudrun.dao"})
 public class WxCloudRunApplication {  
 
   public static void main(String[] args) {
     SpringApplication.run(WxCloudRunApplication.class, args);
+    log.info("小程序后端项目启动成功！！！");
   }
 }
